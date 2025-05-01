00cd26c623491d220bc2e77bd2dec8348add5bb1
Yigantopitekus <157696237+Yigantopitekus@users.noreply.github.com>
2025-05-01 21:10:27 +0200

    Update Search.h

--- ROBERTO/include/Search.h
+++ ROBERTO/include/Search.h
@@ -1,17 +1,3 @@
-// Copyright 2021 Intelligent Robotics Lab
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #ifndef BT_NAV__GETWAYPOINT_HPP_
 #define BT_NAV__GETWAYPOINT_HPP_
 
@@ -22,10 +8,10 @@
 
 #include "geometry_msgs/msg/pose_stamped.hpp"
 
-namespace bt_nav
+namespace library_lib
 {
 
-class GetWaypoint : public BT::ActionNodeBase
+class Search : public BT::ActionNodeBase
 {
 public:
   explicit GetWaypoint(
@@ -45,8 +31,13 @@ public:
 
 private:
   geometry_msgs::msg::PoseStamped wp_;
+  int idx_;
+  rclcpp::Subscription<kobuki_ros_interfaces::msg::ButtonEvent>::SharedPtr button_sub_;
+  std::map<std::string,std::map<std::string,double> waypoints_;
+  int size_;
+  std::string arr_;
 };
 
-}  // namespace bt_nav
+}
 
 #endif  // BT_NAV__GETWAYPOINT_HPP_
